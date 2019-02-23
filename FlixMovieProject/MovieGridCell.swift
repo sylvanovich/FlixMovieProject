34c705e2ac95cc67fcc7bc974fb6882dd8072edb
Joseph Sylvanovich <sylvanovich@Josephs-MBP-2.home>
2019-02-23 15:19:07 -0500

    Preview Movies & Poster View Controller Added

---
+++ FlixMovieProject/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixMovieProject
+//
+//  Created by Joseph Sylvanovich on 2/23/19.
+//  Copyright © 2019 Joseph Sylvanovich. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
---
+++ FlixMovieProject/MoviesDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesDetailViewController.swift
+//  FlixMovieProject
+//
+//  Created by Joseph Sylvanovich on 2/22/19.
+//  Copyright © 2019 Joseph Sylvanovich. All rights reserved.
+//
+
+import UIKit
+
+class MoviesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
